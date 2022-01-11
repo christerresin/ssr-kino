6e8261ff8ae02b1dc16a5d39c12a24756c870f1a
Chrikelin <Christer.resin@gmail.com>
2022-01-11 09:31:23 +0100

    add section for date in footer and logic to dynamicly change its displayed value

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-import express from "express";
+import express from 'express';
 import fs from 'fs/promises';
 
 const app = express();
 
+const currentDate = () => {
+  let myDate = new Date();
+  let myDateString;
+
+  myDate.setDate(myDate.getDate());
+
+  myDateString =
+    myDate.getFullYear() +
+    '-' +
+    ('0' + (myDate.getMonth() + 1)).slice(-2) +
+    '-' +
+    ('0' + myDate.getDate()).slice(-2);
+
+  return myDateString;
+};
+
+currentDate();
+
 app.get('/', async (request, response) => {
   try {
     const dataBuff = await fs.readFile('index.html');
+    const data = dataBuff.toString().replace('$TODAYSDATE$', currentDate());
     response.type('html');
-    response.send(dataBuff);
+    response.send(data);
   } catch (err) {
     console.log(err);
   }
